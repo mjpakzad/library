1b83846bbef42dfadf9788256af5e4d511f1d095
Mojtaba Pakzad <mj.pakzad@gmail.com>
2024-09-07 20:20:30 +0330

    Feat[User]: Add is_staff and is_superuser to user table

---
+++ goodreads/members/migrations/0002_user_is_staff_user_is_superuser.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('members', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='is_staff',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='is_superuser',
+            field=models.BooleanField(default=False),
+        ),
+    ]
