119be80014452267743d086a7293254adae0a051
Mojtaba Pakzad <mj.pakzad@gmail.com>
2024-09-07 08:51:06 +0330

    Fix[IDE]: add .idea to .gitignore

--- goodreads/members/views.py
+++ goodreads/members/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import AuthSerializer
 
-# Create your views here.
+class AuthView(APIView):
+    def post(self, request):
+        serializer = AuthSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response(serializer.errors, status=400)
