d70d3a3045409508c9c974b9d40735477adfe819
Mojtaba Pakzad <mj.pakzad@gmail.com>
2024-09-07 08:52:38 +0330

    Fix[auth]: add members to settings and modify rest framework

--- goodreads/goodreads/settings.py
+++ goodreads/goodreads/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.simplejwt',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'members.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
